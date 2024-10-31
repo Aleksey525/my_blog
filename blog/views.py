16f79f599d4031eff50c3d03a1777c0ea21baf47
Aleksey525 <alex_tolchin@mail.ru>
2024-10-31 11:52:54 +0300

    added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_lst = Post.published.all()
+    paginator = Paginator(post_lst, 2)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
