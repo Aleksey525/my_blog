7fcdb9ae1a60625d0de2bd36a37778973edf4c74
Aleksey525 <alex_tolchin@mail.ru>
2024-12-09 13:49:14 +0300

    create blog_api app

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
     'rest_framework'
 ]
 
